0a773f7243f1c6382cc835e024ffedba91ad0fa1
ilker <ilkerpublic@gmail.com>
2018-05-04 01:43:28 -0400

    enhanced comments

--- src/main/java/ex7/sb/student/jpa/repo/StudentRepository.java
+++ src/main/java/ex7/sb/student/jpa/repo/StudentRepository.java
@@ -24,8 +24,11 @@ import ex7.sb.student.jpa.model.Student;
  *
  */
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+	// NOTE ilker an example "Query method". Below link has many more examples of it
+	//   https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html#jpa.query-methods
 	public Optional<List<Student>> findByName(String name);
 	
+	// NOTE ilker an example "native query using @Query"
 	@Query("select s from STUDENT s where lower(s.name) = lower(:studentName)")
 	public Student findByNameIgnoreCaseQuery(@Param("studentName") String studentName);
 	
